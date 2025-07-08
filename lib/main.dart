51d304664567fb325693eb0bd64fa0296f3bbd7f
olasabry <olas94033@gmail.com>
2025-07-09 01:38:56 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:islamiapp/home_screen.dart';
+
+void main() {
+  runApp(IslamiApp());
+}
+
+class IslamiApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {HomeScreen.routeName: (_) => HomeScreen()},
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
