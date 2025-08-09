ddf4e04c14a892f96b7454b735cbcdc1cfcd4a18
olasabry <olas94033@gmail.com>
2025-08-09 21:20:24 +0300

    Implement sebha$radio

--- lib/tabs/radio_tab.dart
+++ lib/tabs/radio_tab.dart
@@ -1,19 +1,120 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:islamiapp/widgets/radio_item.dart';
 
 class RadioTab extends StatelessWidget {
+  const RadioTab({super.key});
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          "",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-          ),
+    var screenSize = MediaQuery.sizeOf(context);
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xb3202020),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: TabBar(
+                dividerColor: const Color.fromARGB(70, 0, 0, 0),
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicator: BoxDecoration(
+                  color: Color(0xFFE2BE7F),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                unselectedLabelStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal,
+                ),
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                tabs: [
+                  Tab(text: 'Radio'),
+                  Tab(text: 'Reciters'),
+                ],
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8),
+              padding: EdgeInsets.all(8),
+              height: screenSize.height * 0.15,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/details_footer.png'),
+                  fit: BoxFit.fill,
+                ),
+                color: Color(0xFFE2BE7F),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Radio Ibrahim Al-Akdar',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spacer(),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.favorite,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.play_arrow,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.volume_up,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: screenSize.height * 0.02),
+            Container(
+              height: screenSize.height * 0.06,
+              width: double.infinity,
+              child: TabBarView(
+                children: [
+                  ListView.builder(
+                    itemBuilder: (context, index) => RadioItem(),
+                    itemCount: 20,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
--- lib/tabs/sebha/sebha_tab.dart
+++ lib/tabs/sebha/sebha_tab.dart
@@ -1,8 +1,95 @@
 import 'package:flutter/material.dart';
 
-class SebhaTab extends StatelessWidget {
+class SebhaTab extends StatefulWidget {
+  SebhaTab({super.key});
+
+  @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  int counter = 0;
+
+  double angle = 0;
+
+  int index = 0;
+
+  List<String> azkar = [
+    "سبحان الله",
+    "الحمدلله",
+    "الله أكبر",
+    "لا إله إلا الله",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Sebha'));
+    var screenSize = MediaQuery.sizeOf(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Column(
+        children: [
+          Text(
+            "سَبِّحِ اسْمَ رَبِّكَ الأعلى",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: screenSize.height * 0.02),
+          Image.asset('assets/images/sebhatail.png'),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              InkWell(
+                onTap: onClick,
+                child: Transform.rotate(
+                  angle: angle,
+                  child: Image.asset('assets/images/sebhabody.png'),
+                ),
+              ),
+              SizedBox(
+                width: screenSize.width * 0.7,
+
+                child: Column(
+                  children: [
+                    Text(
+                      azkar[index],
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      '${counter}',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onClick() {
+    counter++;
+    angle += 20;
+    if (counter % 33 == 0) {
+      index++;
+    }
+    if (index == azkar.length) {
+      index = 0;
+      counter = 0;
+    }
+    setState(() {});
   }
 }
